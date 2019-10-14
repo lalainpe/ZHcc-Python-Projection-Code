a9891a57c8e5c2e671c31c5f978a7e8f97c576de
lalainpe <35245810+lalainpe@users.noreply.github.com>
2019-10-14 14:57:08 -0700

    Now makes plot of stacked hists

--- ZHcc_Python_Projection_Code.py
+++ ZHcc_Python_Projection_Code.py
@@ -62,17 +62,69 @@ efficiencies = {
     "cl" : Eff_C * Eff_L,
 }
 
-bkgd_stack = ROOT.THStack()
+# bkgd_stack = ROOT.THStack()
 
-for b in sample: 
+# for b in sample: 
 
-    hists_bkgd = hist_bkgd(b, efficiencies)
+#     hists_bkgd = hist_bkgd(b, efficiencies)
 
-    bkgd_stack.Add(hists_bkgd)
-    
-canvas = TCanvas()
-bkgd_stack.Draw('hist')
-canvas.Draw()
+#     bkgd_stack.Add(hists_bkgd)
+
+# canvas = TCanvas()
+# bkgd_stack.Draw('hist')
+# canvas.Draw()
+
+#making plot of the backgrounds, need colors and legend
+
+bkgd_label_stack = ROOT.THStack()
+
+hists_ttbar = hist_bkgd("ttbar_PwPy8_DiLep", efficiencies)
+hists_ZZ = hist_bkgd("ZZ", efficiencies)
+hists_WZ = hist_bkgd("WZ", efficiencies)
+hists_Zjets = hist_bkgd("Zjets", efficiencies)
+hists_VHbb = hist_bkgd("VHbb_NLO", efficiencies)
+
+signal_VHcc = hist_bkgd("VHcc_NLO", efficiencies)
+
+hists_ttbar.SetFillColor(33)
+hists_ZZ.SetFillColor(34)
+hists_WZ.SetFillColor(35)
+hists_Zjets.SetFillColor(40)
+hists_VHbb.SetFillColor(37)
+
+signal_VHcc.SetLineColor(2)
+signal_VHcc.Scale(100)
+
+bkgd_label_stack.Add(hists_ttbar)
+bkgd_label_stack.Add(hists_ZZ)
+bkgd_label_stack.Add(hists_WZ)
+bkgd_label_stack.Add(hists_Zjets)
+bkgd_label_stack.Add(hists_VHbb)
+
+Leg = ROOT.TLegend(0.64,0.9,0.84,0.9-7*0.05)
+
+Leg.SetTextFont(42)
+Leg.SetTextSize(0.04)
+Leg.SetBorderSize(0)
+Leg.SetFillStyle(0)
+
+Leg.AddEntry(hists_ttbar, "ttbar")
+Leg.AddEntry(hists_ZZ, "ZZ")
+Leg.AddEntry(hists_WZ, "WZ")
+Leg.AddEntry(hists_Zjets, "Zjets")
+Leg.AddEntry(hists_VHbb, "VHbb")
+Leg.AddEntry(signal_VHcc, "VHcc")
+
+
+bkgd_canvas = TCanvas()
+bkgd_label_stack.Draw("hist")
+signal_VHcc.Draw('same')
+Leg.Draw()
+bkgd_label_stack.GetYaxis().SetTitle('Events / 10 GeV')
+bkgd_label_stack.GetXaxis().SetTitle('m_{cc} [GeV]')
+bkgd_label_stack.SetTitle("hi_2tag")
+bkgd_canvas.Draw()
+
 
 def signal_significance(Eff_C):
     
@@ -128,7 +180,7 @@ gr_b = ROOT.TGraph()
 
 gr_sb = ROOT.TGraph()
 
-for eff in [x/100. for x in range(0,101,1)]:
+for eff in [x/100. for x in range(20,81,1)]:
     
     x = signal_significance(eff)
     
@@ -138,6 +190,11 @@ for eff in [x/100. for x in range(0,101,1)]:
     
     gr_sb.SetPoint(gr_sb.GetN(), eff, x[2])
 
+#     print("signal = ", x[0], "for Eff_C", eff*100, "%")
+#     print("background = ", x[1], "for Eff_C", eff*100, "%")
+#     print("s/sqrt(b) = ", x[2], "for Eff_C", eff*100, "%")
+#     print("    ")
+                
     
   
 
@@ -158,4 +215,3 @@ gr_b.Draw('ap')
 gr_b.GetYaxis().SetTitle('Background')
 gr_b.GetXaxis().SetTitle('c-tagging eff')
 my_canvas2.Draw()
-
